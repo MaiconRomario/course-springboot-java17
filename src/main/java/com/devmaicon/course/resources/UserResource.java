5afdc282ce1f2b16a8835ffe0622eea9f1d46565
MaiconRomario <maiconromario389@hotmail.com>
2022-09-22 18:05:56 -0300

    H2 database, test profile, JPA

--- src/main/java/com/devmaicon/course/resources/UserResource.java
+++ src/main/java/com/devmaicon/course/resources/UserResource.java
@@ -10,11 +10,10 @@ import com.devmaicon.course.entities.User;
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
 	@GetMapping
 	public ResponseEntity<User> findAll(){
 		User u = new User(1L, "maicon", "maicon@gmail.com", "99999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
-
 }
