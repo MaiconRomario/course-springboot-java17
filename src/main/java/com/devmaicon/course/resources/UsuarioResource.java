dcf2d9a20170dc99aa5a3961a806c03ea2715a11
MaiconRomario <maiconromario389@hotmail.com>
2022-09-24 15:12:58 -0300

    Service layer, component registration

--- src/main/java/com/devmaicon/course/resources/UsuarioResource.java
+++ src/main/java/com/devmaicon/course/resources/UsuarioResource.java
@@ -1,19 +1,33 @@
 package com.devmaicon.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devmaicon.course.entities.Usuario;
+import com.devmaicon.course.services.UsuarioService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UsuarioResource {
+	
+	@Autowired
+	private UsuarioService service;
 
 	@GetMapping
-	public ResponseEntity<Usuario> findAll(){
-		Usuario u = new Usuario(1L, "maicon", "maicon@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Usuario>>findAll(){
+		List<Usuario> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id){
+		Usuario obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
