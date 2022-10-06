1ad0598fba54939453f822d9b323db2411e78c8f
MaiconRomario <maiconromario389@hotmail.com>
2022-10-06 01:04:08 -0300

    Category

---
+++ src/main/java/com/devmaicon/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.devmaicon.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devmaicon.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
